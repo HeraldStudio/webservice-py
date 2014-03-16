d29cf3fcbb226d8e32f3e93cbf840fd3f911d1d6
Goclis <goclisyyh@gmail.com>
2014-03-16 09:27:17 +0800

    Update urls.py

--- herald_web_service/classroom_service/queryEmptyClassrooms/queryEmptyClassrooms/urls.py
+++ herald_web_service/classroom_service/queryEmptyClassrooms/queryEmptyClassrooms/urls.py
@@ -14,17 +14,7 @@ urlpatterns = patterns('',
     (r'^queryEmptyClassrooms/query/([a-z]{3})/([a-z]+)/(\d{1,2})/(\d{1,2})/$', 
         queryEmptyClassrooms.views.querySpecifiedAPI), # 指定【校区，今天（明天），开始，结束】
 
-    # 查询
-	(r'^queryEmptyClassrooms/query/$', queryEmptyClassrooms.views.queryEmptyClassrooms), 
 
-    # 首页
-	(r'^queryEmptyClassrooms/index/$', queryEmptyClassrooms.views.index),
-    (r'^webapp/classroom/$', queryEmptyClassrooms.views.shortIndex), # 方便记忆的链接
-
-    # 关于本站
-    (r'^queryEmptyClassrooms/about/$', queryEmptyClassrooms.views.about),
-   
-    
     # Examples:
     # url(r'^$', 'queryEmptyClassrooms.views.home', name='home'),
     # url(r'^queryEmptyClassrooms/', include('queryEmptyClassrooms.foo.urls')),
