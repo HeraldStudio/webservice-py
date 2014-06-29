4d6c4ddab181cd1c1349c2c9d112509db04dba79
熊海潇 <xindervella@gmail.com>
2014-06-29 18:15:46 +0800

    Update db.py

--- mod/models/db.py
+++ mod/models/db.py
@@ -2,10 +2,10 @@
 # @Date    : 2014-06-25 15:37:23
 # @Author  : xindervella@gamil.com
 
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PWD = '5t@ihu@idi@nn@0'
-DB_NAME = 'webservice'
+DB_HOST = ''
+DB_USER = ''
+DB_PWD = ''
+DB_NAME = ''
 
 from sqlalchemy import create_engine
 
