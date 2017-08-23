3c7daf11124a80b2d0f7d0e6e274bf80fb771a67
Vhyme <vhyme@live.cn>
2017-08-23 23:57:58 +0800

    x

--- mod/schoolbus/new_handler.py
+++ mod/schoolbus/new_handler.py
@@ -10,22 +10,25 @@ class NewSchoolBusHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     @tornado.gen.engine
     def post(self):
-        client = AsyncHTTPClient()
-        request = HTTPRequest('http://121.248.63.119/busservice/lines')
-        response = yield tornado.gen.Task(client.fetch, request)
-        lines = json.loads(response.body)
-        lines['code'] = 200
-        for line in lines['data']['lines']:
-            line_id = line['id']
-            request = HTTPRequest('http://121.248.63.119/busservice/queryBus?lineId=' + str(line_id))
+        try:
+            client = AsyncHTTPClient()
+            request = HTTPRequest('http://121.248.63.119/busservice/lines')
             response = yield tornado.gen.Task(client.fetch, request)
-            buses = json.loads(response.body)
-            line['buses'] = buses['data']['buses']
-            
-            request = HTTPRequest('http://121.248.63.119/busservice/lineDetail?lineId=' + str(line_id))
-            response = yield tornado.gen.Task(client.fetch, request)
-            detail = json.loads(response.body)
-            line['stops'] = detail['data']['line']['linePoints']
-            
-        self.write(json.dumps(lines, ensure_ascii=False))
+            lines = json.loads(response.body)['lines']
+
+            for line in lines:
+                line_id = line['id']
+                request = HTTPRequest('http://121.248.63.119/busservice/queryBus?lineId=' + str(line_id))
+                response = yield tornado.gen.Task(client.fetch, request)
+                buses = json.loads(response.body)
+                line['buses'] = buses['data']['buses']
+                
+                request = HTTPRequest('http://121.248.63.119/busservice/lineDetail?lineId=' + str(line_id))
+                response = yield tornado.gen.Task(client.fetch, request)
+                detail = json.loads(response.body)
+                line['stops'] = detail['data']['line']['linePoints']
+                
+            self.write(json.dumps({ 'code': 200, 'content': lines }, ensure_ascii=False))
+        except:
+            self.write(json.dumps({ 'code': 400, 'content': [] }, ensure_ascii=False))
         self.finish()
